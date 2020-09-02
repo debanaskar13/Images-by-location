910c90048b50a207a5c6ffcd671c50b2c3a16a27
debashis naskar <debanaskar13@gmail.com>
2020-09-02 13:20:18 +0530

    Second Commit

--- app.js
+++ app.js
@@ -6,18 +6,66 @@ $(document).ready(function(){
         $.ajax({
             url:`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=056eaec81e314e348175d4ef37c3fbaf`,
             success:function(data){
-                $('#place').text(`${data.results[0].formatted}`)
+                $('#location-text').text('Your Location is :')
+                $('#place').text(` ${data.results[0].components.suburb},${data.results[0].components.city},${data.results[0].components.state},${data.results[0].components.country},${data.results[0].components.continent}`)
 
                 $.ajax(
                     { 
-                        url: `https://api.unsplash.com/search/photos/?client_id=1PxLhagaDb8qlR8vyKKTBy7modcHos4-l5-GXvJV_Lk&query=${data.results[0].formatted}`,
+                        url: `https://api.unsplash.com/search/photos/?client_id=1PxLhagaDb8qlR8vyKKTBy7modcHos4-l5-GXvJV_Lk&query=${data.results[0].components.state}`,
 
                         success:function(result){
                             for (let i in result.results){
-
                                 $('#image-box').append(
                                     `<div class="col-lg-3 col-md-4 col-sm-6 col-12">
-                                        <img src="${result.results[i].urls.full}" alt="" class="img-fluid mb-4" style="width:100%;height:200px; background-size:cover">
+                                        <img src="${result.results[i].urls.regular}" alt="" class="img-fluid mb-4" style="width:100%;height:200px; background-size:cover">
+                                    </div>`
+                                )
+                            }
+                            
+                        }
+                    }
+                )
+                $.ajax(
+                    { 
+                        url: `https://api.unsplash.com/search/photos/?client_id=1PxLhagaDb8qlR8vyKKTBy7modcHos4-l5-GXvJV_Lk&query=${data.results[0].components.country}`,
+
+                        success:function(result){
+                            for (let i in result.results){
+                                $('#image-box').append(
+                                    `<div class="col-lg-3 col-md-4 col-sm-6 col-12">
+                                        <img src="${result.results[i].urls.regular}" alt="" class="img-fluid mb-4" style="width:100%;height:200px; background-size:cover">
+                                    </div>`
+                                )
+                            }
+                            
+                        }
+                    }
+                )
+                $.ajax(
+                    { 
+                        url: `https://api.unsplash.com/search/photos/?client_id=1PxLhagaDb8qlR8vyKKTBy7modcHos4-l5-GXvJV_Lk&query=${data.results[0].components.state_district}`,
+
+                        success:function(result){
+                            for (let i in result.results){
+                                $('#image-box').append(
+                                    `<div class="col-lg-3 col-md-4 col-sm-6 col-12">
+                                        <img src="${result.results[i].urls.regular}" alt="" class="img-fluid mb-4" style="width:100%;height:200px; background-size:cover">
+                                    </div>`
+                                )
+                            }
+                            
+                        }
+                    }
+                )
+                $.ajax(
+                    { 
+                        url: `https://api.unsplash.com/search/photos/?client_id=1PxLhagaDb8qlR8vyKKTBy7modcHos4-l5-GXvJV_Lk&query=${data.results[0].components.suburb}`,
+
+                        success:function(result){
+                            for (let i in result.results){
+                                $('#image-box').append(
+                                    `<div class="col-lg-3 col-md-4 col-sm-6 col-12">
+                                        <img src="${result.results[i].urls.regular}" alt="" class="img-fluid mb-4" style="width:100%;height:200px; background-size:cover">
                                     </div>`
                                 )
                             }
@@ -34,6 +82,15 @@ $(document).ready(function(){
             }
         })
     
+    },
+    function(error){
+        let response=confirm('Allow Location to see Images')
+        if(response){
+            $('#place').text("Please Refresh your page and Allow your location...")
+        }
+        else {
+            $('#place').text("You didn't give permission to check your location")
+        }
     })
 })
 
