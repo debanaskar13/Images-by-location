0b0d8c2ce6dd0c2c7246fcaf02c06226485edcf8
Debashis Naskar <56424991+debanaskar13@users.noreply.github.com>
2020-09-03 12:25:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
             url:`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=056eaec81e314e348175d4ef37c3fbaf`,
             success:function(data){
                 $('#location-text').text('Your Location is :')
-                $('#place').text(` ${data.results[0].components.suburb},${data.results[0].components.city},${data.results[0].components.state},${data.results[0].components.country},${data.results[0].components.continent}`)
+                $('#place').text(` ${data.results[0].formatted}`)
 
                 $.ajax(
                     { 
@@ -41,41 +41,6 @@ $(document).ready(function(){
                         }
                     }
                 )
-                $.ajax(
-                    { 
-                        url: `https://api.unsplash.com/search/photos/?client_id=1PxLhagaDb8qlR8vyKKTBy7modcHos4-l5-GXvJV_Lk&query=${data.results[0].components.state_district}`,
-
-                        success:function(result){
-                            for (let i in result.results){
-                                $('#image-box').append(
-                                    `<div class="col-lg-3 col-md-4 col-sm-6 col-12">
-                                        <img src="${result.results[i].urls.regular}" alt="" class="img-fluid mb-4" style="width:100%;height:200px; background-size:cover">
-                                    </div>`
-                                )
-                            }
-                            
-                        }
-                    }
-                )
-                $.ajax(
-                    { 
-                        url: `https://api.unsplash.com/search/photos/?client_id=1PxLhagaDb8qlR8vyKKTBy7modcHos4-l5-GXvJV_Lk&query=${data.results[0].components.suburb}`,
-
-                        success:function(result){
-                            for (let i in result.results){
-                                $('#image-box').append(
-                                    `<div class="col-lg-3 col-md-4 col-sm-6 col-12">
-                                        <img src="${result.results[i].urls.regular}" alt="" class="img-fluid mb-4" style="width:100%;height:200px; background-size:cover">
-                                    </div>`
-                                )
-                            }
-                            
-                        }
-                    }
-                )
-                
-
-            },
             error:function(data){
                 $('body').html(data)
                 console.log("error")
